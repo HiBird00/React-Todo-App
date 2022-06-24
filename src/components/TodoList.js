5e429c7870ab1a4e89056b63d4ecc5a6bb478ae0
SuHyeon Kim <kim113355@naver.com>
2022-06-25 05:57:05 +0900

    react memo, react-virtualized로 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import "./TodoList.scss";
+import { List } from 'react-virtualized';
 import TodoListItem from "./TodoListItem";
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+    const rowRenderer = useCallback(({ index, key, style }) => {
+        const todo = todos[index];
+        return (
+            <TodoListItem
+                todo={todo}
+                key={todo.id}
+                onRemove={onRemove}
+                onToggle={onToggle}
+                style={style}
+            />
+        )
+    }, [onRemove, onToggle, todos])
+
+
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-            ))}
-        </div>
+        <List
+            className="TodoList"
+            width={512} // 전체 크기
+            height={513} // 전체 높이
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{ outline: 'none' }}
+        />
     );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
